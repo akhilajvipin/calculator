e3ac2f43a56b5e71e72d61789d1d2fc8ebad3711
akhilajvipin <akhilaanthikkatt95@gmail.com>
2024-02-03 11:54:15 +0530

    calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function display(num){
+    result.value+=num
+}
+function equal(){
+    try{result.value=eval(result.value)}
+    catch{
+        result.value="error"
+        setTimeout(()=>{result.value=""},1000)
+    }
+}
+function clearall(){
+    result.value=""
+}
+function back(){
+    result.value=result.value.slice(0,-1)
+}
